859096bf0eb6abc0bcb5bfcd960c72504a9bca52
mgengibirra <104502312+mgengibirra@users.noreply.github.com>
2023-05-25 08:46:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import streamlit as st
 st.title("File Manager")
 
 selected_dir = st.text_input("Enter the path to your directory:")
-
+try:
+    os.chdir(selected_dir)
+    st.success("Current working directory set to: " + os.getcwd())
+except OSError:
+    st.error("Invalid directory path.")
+        
 '''
 # Streamlit app    
     
