fd8df35068d1d315ae2386482d49ae1ce11891ba
mgengibirra <104502312+mgengibirra@users.noreply.github.com>
2023-05-25 09:22:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import streamlit as st
 
 st.title("File Manager")
 
-selected_dir = st.text_input("Enter the path to your directory:")
+endereço = st.text_input("Enter the path to your directory:")
+endereço = r"%s"%endereço
 
 try:
-    abs_dir = os.path.abspath(selected_dir)
+    abs_dir = os.path.abspath(endereço)
     os.chdir(abs_dir)
     st.success("Current working directory set to: " + os.getcwd())
 except OSError:
