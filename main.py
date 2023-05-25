6b9d55e339a4fac4a0fd964c37ac61482b7de1b8
mgengibirra <104502312+mgengibirra@users.noreply.github.com>
2023-05-25 08:42:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import os
+#import os
 import streamlit as st
 
-# Streamlit app
-def main():
-    st.title("File Manager")
-    
-    # Directory selection
-    selected_dir = st.text_input("Enter the path to your directory:")
-    if selected_dir:
-        try:
-            os.chdir(selected_dir)
-            st.success("Current working directory set to: " + os.getcwd())
-        except OSError:
-            st.error("Invalid directory path.")
-    
-    # File selection
-    file_list = os.listdir(os.getcwd())
-    selected_file = st.selectbox("Choose a file to open", file_list)
-    if selected_file:
-        try:
-            with open(selected_file, 'r') as file:
-                contents = file.read()
-            st.write(contents)
-        except FileNotFoundError:
-            st.error(f"File '{selected_file}' not found.")
+st.title("File Manager")
 
-if __name__ == '__main__':
-    main()
+'''
+# Streamlit app    
+    
+# Directory selection
+selected_dir = st.text_input("Enter the path to your directory:")
+if selected_dir:
+    try:
+        os.chdir(selected_dir)
+        st.success("Current working directory set to: " + os.getcwd())
+    except OSError:
+        st.error("Invalid directory path.")
+
+# File selection
+file_list = os.listdir(os.getcwd())
+selected_file = st.selectbox("Choose a file to open", file_list)
+if selected_file:
+    try:
+        with open(selected_file, 'r') as file:
+            contents = file.read()
+        st.write(contents)
+    except FileNotFoundError:
+        st.error(f"File '{selected_file}' not found.")
+
+'''
